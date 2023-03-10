ff08dea80152be974702034c7a34aa4a21ed0600
hjonin <helene.jonin@data.gouv.fr>
2023-03-10 12:24:08 +0100

    remove useless var

--- example/to-file/index.ts
+++ example/to-file/index.ts
@@ -6,7 +6,6 @@ import {todoistToMarkdown} from "../../index";
 dotenv.config({ path: `${__dirname}/.env` })
 
 todoistToMarkdown(process.env.TOKEN as string).then((projects: any[]) => {
-    let i = 1;
     for (const project of projects) {
         fs.writeFile(`${__dirname}/${project.project_name}.md`, project.project_tree);
     }
